78d76b56442b6cd2d43a182b8b227b5ec30d4781
Rayna MacFarlane <rmacfarlane43@gmail.com>
2022-09-21 14:12:29 -0700

    Add places to visit

--- friends.js
+++ friends.js
@@ -2,3 +2,7 @@ var arrays = ["Amy", "Tom", "Mary", "Bob", "Ash"];
 console.log(arrays[3]);
 console.log(arrays[4]);
 console.log(arrays[0]);
+
+var placesToVisit = ["Maine", "Alaska", "Thailand", "Vegas", "Colorado"];
+
+console.log(placesToVisit.shift());
